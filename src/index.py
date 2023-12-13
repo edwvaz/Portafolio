9dfacab20e09cd942b5d03d1bcc9d64eae15d284
edwvaz <edwvazmor@gmail.com>
2023-12-13 16:08:32 -0300

    Subir portafolio sin db

--- src/index.py
+++ src/index.py
@@ -3,18 +3,18 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
 #url_for es una función de Flask que genera una URL para la función especificada.
 #render_template: te lleva a un archivo html
-from flask_mysqldb import MySQL
+#from flask_mysqldb import MySQL
 # __name__:variable que me da python, es para confirmar que estoy en el archivo principal, quiere decir que es el archivo que me va a arrancar la aplicación
 app = Flask(__name__)
 # Vamos a hacer dos rutas, la función route() es para hacer rutas
 
 
 #Mysql Connection
-app.config['MYSQL_HOST']='localhost'
-app.config['MYSQL_USER']='root'
-app.config['MYSQL_PASSWORD']=''
-app.config['MYSQL_DB']='flaskcontacts'
-mysql = MySQL(app)
+#app.config['MYSQL_HOST']='localhost'
+#app.config['MYSQL_USER']='root'
+#app.config['MYSQL_PASSWORD']=''
+#app.config['MYSQL_DB']='flaskcontacts'
+#mysql = MySQL(app)
 
 
 @app.route("/")  # cuando se pone ("/") quiere decir que es mi pagina principal
@@ -57,81 +57,75 @@ def inteligenciaartificial():
 
 
 
-@app.route('/Dataengineer')
-def dataengineer():
-    cur=mysql.connection.cursor()
-    cur.execute('SELECT * FROM contacts')
-    data=cur.fetchall()
+#@app.route('/Dataengineer')
+#def dataengineer():
+#    cur=mysql.connection.cursor()
+#    cur.execute('SELECT * FROM contacts')
+#    data=cur.fetchall()
      # Aqui hacemos la consulta para que aparezcan los datos que ya han sido agregados a la tabla
-    return render_template('dataengineer.html', contacts=data)
+#    return render_template('dataengineer.html', contacts=data)
 
 # settings: es para decirle, como va ir proteguida nuestra cesión
 app.secret_key = "mysecretkey"
 
 
-@app.route('/Dataengineer/add_contact', methods=['POST'])
-def add_contact():
-    if request.method == 'POST':
-        fullname = request.form['fullname']
-        phone = request.form['phone']
-        email = request.form['email']
-        cur=mysql.connection.cursor() #esto se llama cursor, y es para saber donde la conexión (mas o menos es eso)
-        cur.execute('INSERT INTO contacts (fullname, phone, email) VALUES (%s,%s,%s)',
-                    (fullname, phone, email)) # aqui se "Escriben" las consultas sql select, into, insert
-        # INSERT INTO contacts : significa "Insertar dentro de contactos"
-        mysql.connection.commit() # Aquí se ejecutan las consultas y en este caso se guardan las consultas
-        flash('Contact Added successfully') #Esto "flash es para agregar un mensaje"
-        print(fullname)
-        print(phone)
-        print(email)
-        return redirect(url_for('dataengineer')) 
+#@app.route('/Dataengineer/add_contact', methods=['POST'])
+#def add_contact():
+#    if request.method == 'POST':
+#        fullname = request.form['fullname']
+#        phone = request.form['phone']
+#        email = request.form['email']
+#        cur=mysql.connection.cursor() #esto se llama cursor, y es para saber donde la conexión (mas o menos es eso)
+#        cur.execute('INSERT INTO contacts (fullname, phone, email) VALUES (%s,%s,%s)',
+#                    (fullname, phone, email)) # aqui se "Escriben" las consultas sql select, into, insert
+#        # INSERT INTO contacts : significa "Insertar dentro de contactos"
+#        mysql.connection.commit() # Aquí se ejecutan las consultas y en este caso se guardan las consultas
+#        flash('Contact Added successfully') #Esto "flash es para agregar un mensaje"
+#        print(fullname)
+#        print(phone)
+#        print(email)
+#        return redirect(url_for('dataengineer')) 
     # lo que hace con "return" esto es retornar a la vista de index nuevamente para seguir cargando datos
 
 #,methods=['GET', 'POST']
 
-@app.route('/Dataengineer/edit/<id>')
-def get_contact(id):
-    cur=mysql.connection.cursor()
-    cur.execute('SELECT * FROM contacts WHERE id= %s', [id])
-    data = cur.fetchall()
-    return render_template('edit-contact.html', contact =data[0])
+#@app.route('/Dataengineer/edit/<id>')
+#def get_contact(id):
+#    cur=mysql.connection.cursor()
+#    cur.execute('SELECT * FROM contacts WHERE id= %s', [id])
+#    data = cur.fetchall()
+#    return render_template('edit-contact.html', contact =data[0])
 
 #,methods=['POST']
-@app.route('/Dataengineer/update/<id>',methods=['POST'])
-def update_contact(id):
-    if request.method == 'POST':
-        fullname =request.form['fullname']
-        phone =request.form['phone']
-        email =request.form['email']
-        cur=mysql.connection.cursor()
-        cur.execute("""UPDATE contacts
-                    SET fullname=%s,
-                    phone=%s,
-                    email=%s
-                    WHERE id=%s
-                    """,(fullname, phone, email, id))
-        mysql.connection.commit()
-        flash('Contact Update Succesffuly')
-        return redirect(url_for('dataengineer'))
+#@app.route('/Dataengineer/update/<id>',methods=['POST'])
+#def update_contact(id):
+#    if request.method == 'POST':
+#        fullname =request.form['fullname']
+#        phone =request.form['phone']
+#        email =request.form['email']
+#        cur=mysql.connection.cursor()
+#        cur.execute("""UPDATE contacts
+#                    SET fullname=%s,
+#                    phone=%s,
+#                    email=%s
+#                    WHERE id=%s
+#                    """,(fullname, phone, email, id))
+#        mysql.connection.commit()
+#        flash('Contact Update Succesffuly')
+#        return redirect(url_for('dataengineer'))
 
 
-@app.route('/Dataengineer/delete/<string:id>') 
+#@app.route('/Dataengineer/delete/<string:id>') 
 # Esta ruta delete recibe un parametro string de tipo id. 
 # De esta manera le digo que cuando reciba una ruta delete
 # debe tener al lado un id para luego nosotros usar ese numero para poder borrarlo
 
-def delete_contact(id):
-    cur=mysql.connection.cursor() #Aquí obtenemos el cursor
-    cur.execute('DELETE FROM contacts WHERE id={0}'.format(id)) # Aquí ejecutamos hacemos la consulta
-    mysql.connection.commit()
-    flash('Contact Removed Successfully')
-    return redirect(url_for('dataengineer'))
-    
-
-
-
-
-
+#def delete_contact(id):
+#    cur=mysql.connection.cursor() #Aquí obtenemos el cursor
+#    cur.execute('DELETE FROM contacts WHERE id={0}'.format(id)) # Aquí ejecutamos hacemos la consulta
+#    mysql.connection.commit()
+#    flash('Contact Removed Successfully')
+#    return redirect(url_for('dataengineer'))
 
 
 
--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Edwin\Desktop\Portafolio\venv
+set VIRTUAL_ENV=E:\Edwin\Portafolio sin DB\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
